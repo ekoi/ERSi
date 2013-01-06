31336627a3ec9a80250babaa81c40cf8e136134a
Eko Indarto on Mac <eko.indarto@dans.knaw.nl>
2013-01-06 10:54:18 +0100

    data selection and data prep processor modules are added. There are the basics of this project.

---
+++ data-preprocessor/src/main/java/nl/knaw/dans/ersi/datapreprocessor/App.java
@@ -0,0 +1,13 @@
+package nl.knaw.dans.ersi.datapreprocessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
